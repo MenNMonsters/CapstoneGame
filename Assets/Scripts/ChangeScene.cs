eda1eeaaafacd5069b3b3bac09c1f6b714ae0a64
Da <alienld2014@gmail.com>
2017-03-11 15:57:44 -0500

    Menu Control
    Added different menus(scenes) and transition between those scenes.

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour {
+
+    public void changeToScene(int changeTheScene) {
+        SceneManager.LoadScene(changeTheScene);
+    }
+
+    public void quitScene() {
+        Application.Quit();
+    }
+	
+}
