b372fa95f1456180b661e89757b5aeb7f1f6d462
Da <alienld2014@gmail.com>
2017-03-25 14:46:42 -0400

    Added a lobby and a simple 2D networking game
    Added a lobby which allows one user to create a lobby as server and
    other users to join that lobby. After everyone prepares, game will
    start.
    Added a simple 2D networking game which allows users to use keyboard(A,
    D and Space) to control their characters to play with others.
    Added transitions between all the scenes.

---
+++ Assets/PlayerNetworkSetup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerNetworkSetup : NetworkBehaviour {
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<Controller2D>().enabled = true;
+        GetComponent<Player>().enabled = true;
+    }
+
+}
