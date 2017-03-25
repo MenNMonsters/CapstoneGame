9a34922993a0e4ccf3355f017096c5d3909a2ceb
Da <alienld2014@gmail.com>
2017-03-25 00:50:46 -0400

    Changed resolusion
    Adjusted screen resolusion but still not perfect.

---
+++ Assets/MainMenu/ChangeScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Camera.main.projectionMatrix = Matrix4x4.Ortho(-5.0f * 259f / 194f, 5.0f * 259f / 194f, -5.0f, 0.5f, 0.3f, 1000f);
+        // SceneManager.LoadScene(1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
