eb6b4dcedaf14d69ae5f026ed71113881191813b
hunterlin3 <mereloye@gmail.com>
2017-04-14 20:07:28 -0400

    Password protected games properly working
    Game lobbies can now have a password in order to enter them.
    
    NOTE: You will always be prompted for a password even if the game is
    public. To enter public games, you just submit an empty string.

--- Assets/Lobby/Scripts/Lobby/LobbyServerEntry.cs
+++ Assets/Lobby/Scripts/Lobby/LobbyServerEntry.cs
@@ -7,32 +7,62 @@ using System.Collections;
 
 namespace Prototype.NetworkLobby
 {
-    public class LobbyServerEntry : MonoBehaviour 
+    public class LobbyServerEntry : MonoBehaviour
     {
         public Text serverInfoText;
         public Text slotInfo;
         public Button joinButton;
 
-		public void Populate(MatchInfoSnapshot match, LobbyManager lobbyManager, Color c)
-		{
+        LobbyServerList lobbyServerList;
+        LobbyManager lobbyManager;
+        ulong networkID;
+
+        public void Populate(MatchInfoSnapshot match, LobbyManager lm, Color c, LobbyServerList lobbySL)
+        {
             serverInfoText.text = match.name;
 
             slotInfo.text = match.currentSize.ToString() + "/" + match.maxSize.ToString(); ;
 
-            NetworkID networkID = match.networkId;
+            networkID = (ulong) match.networkId;
+
+            lobbyManager = lm;
+
+            lobbyServerList = lobbySL;
 
             joinButton.onClick.RemoveAllListeners();
-            joinButton.onClick.AddListener(() => { JoinMatch(networkID, lobbyManager); });
+            joinButton.onClick.AddListener(() => { onJoinButtonClick(); });
 
             GetComponent<Image>().color = c;
         }
 
-        void JoinMatch(NetworkID networkID, LobbyManager lobbyManager)
+        public void onJoinButtonClick()
         {
-			lobbyManager.matchMaker.JoinMatch(networkID, "", "", "", 0, 0, lobbyManager.OnMatchJoined);
-			lobbyManager.backDelegate = lobbyManager.StopClientClbk;
+            lobbyServerList.showPasswordPromptPanel();
+
+            lobbyServerList.submitButton.onClick.RemoveAllListeners();
+            lobbyServerList.submitButton.onClick.AddListener(() => { JoinMatch(); });
+
+            lobbyServerList.passwordInput.onEndEdit.RemoveAllListeners();
+            lobbyServerList.passwordInput.onEndEdit.AddListener(onEndEditPassword);
+        }
+
+        void JoinMatch()
+        {
+            string password = lobbyServerList.passwordInput.text;
+            lobbyServerList.passwordInput.text = "";
+
+            lobbyManager.matchMaker.JoinMatch((NetworkID) networkID, password, "", "", 0, 0, lobbyManager.OnMatchJoined);
+            lobbyManager.backDelegate = lobbyManager.StopClientClbk;
             lobbyManager._isMatchmaking = true;
             lobbyManager.DisplayIsConnecting();
+        }
+
+        void onEndEditPassword(string text)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                JoinMatch();
+            }
         }
     }
 }
