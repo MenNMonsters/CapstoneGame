9f7740ff25f0ac4b2dc4862c30934c4dfdc74579
Dev <devdmaru@gmail.com>
2017-04-22 23:05:10 -0400

    Changes made in movement and combat

--- Assets/Game/Combat/Movement.cs
+++ Assets/Game/Combat/Movement.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Movement : NetworkBehaviour {
-	
+	public GameObject mainCamera;
 	float x; 
 	float y;
 	// Use this for initialization
 	void Start () {
-		
+		mainCamera = GameObject.Find ("Canvas");
+
 	}
 	
 	// Update is called once per frame
@@ -20,12 +22,6 @@ public class Movement : NetworkBehaviour {
 
 		 x = Input.GetAxis ("Horizontal") * Time.deltaTime * 3.0f;
 		 y = Input.GetAxis ("Vertical") * Time.deltaTime * 3.0f;
-
-
-
-		
-
-		//transform.Rotate (0, x, 0);
 		
 		}
 		transform.Translate (x, y, 0);
@@ -33,14 +29,21 @@ public class Movement : NetworkBehaviour {
 
 
 
-	void OnGUI(){
+	/*void OnGUI(){
 		if (GUI.Button(new Rect(Screen.width * (85f / 100f), Screen.height * (1f * 0.83f), Screen.width * (0.1f), Screen.height * (0.065f)), "PASS"))
 		{
-			NetworkManager.singleton.ServerChangeScene("MainMenu");
+			
+			//SceneManager.LoadScene(1);
+			//NetworkManager.singleton.ServerChangeScene("Combat");
+			//mainCamera.transform.Translate(1,1,0);
+			//Destroy(GameObject.Find("Network Manager"));
+			//NetworkManager.Shutdown();
+			//SceneManager.LoadScene("Combat", LoadSceneMode.Single);
+
 		}
 	
 	
-	}
+	}*/
 
 
 }
